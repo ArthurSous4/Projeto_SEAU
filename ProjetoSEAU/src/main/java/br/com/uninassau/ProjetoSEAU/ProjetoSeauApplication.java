dae9fc4727f837ca2ec0a4bafb0473f17a2c7bd1
Pedro Luiz <pedrolcavalcanteplcfg.2020@gmail.com>
2024-10-31 17:06:23 -0300

    função buscar catalogo de filmes

--- ProjetoSEAU/src/main/java/br/com/uninassau/ProjetoSEAU/ProjetoSeauApplication.java
+++ ProjetoSEAU/src/main/java/br/com/uninassau/ProjetoSEAU/ProjetoSeauApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ProjetoSeauApplication implements CommandLineRunner {
 
@@ -13,6 +19,17 @@ public class ProjetoSeauApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		Scanner leitura = new Scanner(System.in);
+		System.out.println("Digite um filme para busca: ");
+		var busca = leitura.nextLine();
+
+		String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=700745a5";
+
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create(endereco))
+				.build();
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		System.out.println(response.body());
 	}
 }
